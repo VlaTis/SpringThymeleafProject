79ddc67f8b0f3498518593c6fb1c3a51d0c127a8
Vladas Tiskus <vl.tiskus@gmail.com>
2022-05-12 20:16:47 +0300

    Initial commit

---
+++ vteshop/src/main/java/eu/codeacademy/vteshop/VteshopApplication.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.vteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VteshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VteshopApplication.class, args);
+    }
+
+}
