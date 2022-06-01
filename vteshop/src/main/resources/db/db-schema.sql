49e4fb56792e69bd7836d4417571bea4ed2e9cdc
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-01 20:34:35 +0300

    [2][DB][Recourses] Created: configured and created db-schema and initial-data for H2, to use some demo data. + Some minor changes in product template

--- vteshop/src/main/resources/db/db-schema.sql
+++ vteshop/src/main/resources/db/db-schema.sql
@@ -1,8 +1,65 @@
-CREATE TABLE PRODUCT (
-                         id INT PRIMARY KEY,
-                         product_id UUID NOT NULL,
-                         name VARCHAR(20) NOT NULL,
-                         quantity_in_stock INT NOT NULL,
-                         price DECIMAL(20, 2) NOT NULL,
-                         description VARCHAR(500) DEFAULT NULL
+DROP TABLE IF EXISTS operation_station;
+CREATE TABLE operation_station
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    CONSTRAINT operation_station_key UNIQUE (name)
 );
+
+DROP TABLE IF EXISTS product_category;
+CREATE TABLE product_category
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    CONSTRAINT product_category_key UNIQUE (name)
+);
+
+DROP TABLE IF EXISTS product_status;
+CREATE TABLE product_status
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    CONSTRAINT product_status_key UNIQUE (name)
+);
+DROP TABLE IF EXISTS product;
+CREATE TABLE product
+(
+    id                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    product_id        UUID           NOT NULL,
+    name              VARCHAR(20)    NOT NULL,
+    quantity_in_stock INT            NOT NULL,
+    price             DECIMAL(20, 2) NOT NULL,
+    description       VARCHAR(500) DEFAULT NULL,
+    operation_station BIGINT references operation_station(id),
+    product_status    BIGINT references product_status(id),
+    product_category    BIGINT references product_category(id)
+
+);
+
+
+DROP TABLE IF EXISTS production_order_status;
+CREATE TABLE production_order_status
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    CONSTRAINT production_order_status_key UNIQUE (name)
+);
+
+DROP TABLE IF EXISTS production_order;
+CREATE TABLE production_order
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    quantity   INT NOT NULL,
+    production_order_status  BIGINT references production_order_status(id),
+    product_id  BIGINT references product(id)
+
+
+);
+
+
+
+
+
+
+
---
+++ vteshop/src/main/resources/db/initial-data.sql
@@ -0,0 +1,39 @@
+INSERT INTO product_category(name)
+VALUES ('Shafts'),
+       ('Bearings'),
+       ('Couplings');
+
+
+INSERT INTO operation_station(name)
+VALUES ('Shaft and Coupling Machining'),
+       ('Bearings production');
+
+
+INSERT INTO product_status(name)
+VALUES ('Available'),
+       ('Not available');
+
+INSERT INTO product (product_id, name, quantity_in_stock, price, description, operation_station, product_status,
+                     product_category)
+VALUES ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'Shaft1', 200, 100, 'Standard copper shaft', 1, 1, 1),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Shaft2', 50, 120, 'Standard alu shaft', 1, 1, 1),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991343', 'Shaft3', 20, 140, 'Standard alu shaft, Large', 1, 1, 1),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Shaft4', 200, 150, 'Standard copper shaft, Large', 1, 1, 1),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Bearing1', 150, 100, 'Standard ball bearing', 2, 1, 2),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Bearing2', 200, 50, 'Standard ball bearing with rings', 2, 1, 2),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991347', 'Bearing3', 500, 50, 'Standard needle bearing', 2, 1, 2),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991348', 'Bearing4', 20, 70, 'Standard needle bearing with rings', 2, 1, 2),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991358', 'Bearing5', 20, 200, 'Standard ball bearing Large', 2, 2, 2),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991368', 'Coupling1', 20, 100, 'Standard coupling', 1, 1, 3),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991378', 'Coupling2', 20, 150, 'Standard coupling with bumper', 1, 1, 3),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991388', 'Coupling3', 20, 200, 'Standard coupling Large', 1, 1, 3),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991349', 'Coupling4', 16, 100, 'Standard coupling Extra Large', 1, 1, 3);
+
+
+
+
+
+
+
+
+
