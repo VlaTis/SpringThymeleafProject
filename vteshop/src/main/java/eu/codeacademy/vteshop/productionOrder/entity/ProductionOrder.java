844f2f9a5a19adcf4e9a105a088ae3a992c0e359
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-01 20:31:23 +0300

    [2][Spring][Entities] Created: ProductionOrder and ProductionOrderStatus entities with relationships.

---
+++ vteshop/src/main/java/eu/codeacademy/vteshop/productionOrder/entity/ProductionOrder.java
@@ -0,0 +1,32 @@
+package eu.codeacademy.vteshop.productionOrder.entity;
+
+import eu.codeacademy.vteshop.product.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductionOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "production_order_status")
+    private ProductionOrderStatus productionOrderStatus;
+
+
+}
---
+++ vteshop/src/main/java/eu/codeacademy/vteshop/productionOrder/entity/ProductionOrderStatus.java
@@ -0,0 +1,25 @@
+package eu.codeacademy.vteshop.productionOrder.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductionOrderStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "productionOrderStatus")
+    private Set<ProductionOrder> productionOrders;
+}
