8845960f6476185c237e39a2ff7121732f838a5d
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-01 20:40:22 +0300

    [Spring][DTO] Created: OperationStationDto

---
+++ vteshop/src/main/java/eu/codeacademy/vteshop/operationStation/entity/dto/OperationStationDto.java
@@ -0,0 +1,10 @@
+package eu.codeacademy.vteshop.operationStation.entity.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OperationStationDto {
+    private String name;
+}
