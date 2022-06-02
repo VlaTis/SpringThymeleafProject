a98b107686c6688b569dacd7b580336044c98726
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-03 01:35:44 +0300

    [Spring] GlobalModelAdvice added

---
+++ vteshop/src/main/java/eu/codeacademy/vteshop/advice/GlobalModelAdvice.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.vteshop.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAdvice {
+
+    @ModelAttribute("currency")
+    public char currency() {
+        return '€';
+    }
+}
