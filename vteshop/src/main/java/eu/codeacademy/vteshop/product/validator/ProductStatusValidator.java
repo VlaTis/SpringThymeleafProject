63743320346be99aa7ff12a65b59af6a3457f342
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-06 18:44:21 +0300

    [FE] fixed visual bug with page buttons

--- vteshop/src/main/java/eu/codeacademy/vteshop/product/validator/ProductStatusValidator.java
+++ vteshop/src/main/java/eu/codeacademy/vteshop/product/validator/ProductStatusValidator.java
@@ -1,13 +1,9 @@
 package eu.codeacademy.vteshop.product.validator;
 
-
-import eu.codeacademy.vteshop.product.repository.ProductCategoryRepository;
 import eu.codeacademy.vteshop.product.repository.ProductStatusRepository;
 import lombok.RequiredArgsConstructor;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
 
 @RequiredArgsConstructor
 public class ProductStatusValidator implements ConstraintValidator<ProductStatusValid, String> {
@@ -20,9 +16,6 @@ public class ProductStatusValidator implements ConstraintValidator<ProductStatus
 
     @Override
     public boolean isValid(String productStatusName, ConstraintValidatorContext context) {
-
-
-
         return productStatusName != null && productStatusRepository.findProductStatusByName(productStatusName).isPresent();
     }
 }
