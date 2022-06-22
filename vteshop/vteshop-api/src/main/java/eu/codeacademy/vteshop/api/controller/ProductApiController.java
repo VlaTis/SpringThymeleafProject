f13313979c477c7ba21da4394da41b925e7087b6
Vladas Tiskus <vl.tiskus@gmail.com>
2022-06-22 18:18:51 +0300

    [Modules] moved resources to vteshop-main

--- vteshop/vteshop-api/src/main/java/eu/codeacademy/vteshop/api/controller/ProductApiController.java
+++ vteshop/vteshop-api/src/main/java/eu/codeacademy/vteshop/api/controller/ProductApiController.java
@@ -3,6 +3,7 @@ package eu.codeacademy.vteshop.api.controller;
 
 import eu.codeacademy.vteshop.common.product.dto.ProductDto;
 import eu.codeacademy.vteshop.common.product.service.ProductService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
+@Api
 public class ProductApiController {
 
     private final ProductService productService;
