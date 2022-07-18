3cbfdcca69afd02ab97aa1ca91f4b874a8738650
Vladas Tiskus <vl.tiskus@gmail.com>
2022-07-18 22:43:37 +0300

    [Bug fixing] returned accidentally removed files

--- vteshop/vteshop-mvc/src/main/java/eu/codeacademy/vteshop/config/ApplicationConfig.java
+++ vteshop/vteshop-mvc/src/main/java/eu/codeacademy/vteshop/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package config;
+package eu.codeacademy.vteshop.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
--- vteshop/vteshop-mvc/src/main/java/eu/codeacademy/vteshop/config/SecurityConfig.java
+++ vteshop/vteshop-mvc/src/main/java/eu/codeacademy/vteshop/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package config;
+package eu.codeacademy.vteshop.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
