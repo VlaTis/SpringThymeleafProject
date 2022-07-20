da4bb6d7877794c7cd513f522aafb083887cfbb7
Vladas Tiskus <vl.tiskus@gmail.com>
2022-07-20 22:42:30 +0300

    [SalesOrder-Controller] add and get features added

--- vteshop/vteshop-api/src/main/java/eu/codeacademy/vteshop/api/controller/orders/sales/SalesOrderApiController.java
+++ vteshop/vteshop-api/src/main/java/eu/codeacademy/vteshop/api/controller/orders/sales/SalesOrderApiController.java
@@ -4,11 +4,13 @@ package eu.codeacademy.vteshop.api.controller.orders.sales;
 import eu.codeacademy.vteshop.common.orders.sales.dto.SalesOrderDto;
 import eu.codeacademy.vteshop.common.orders.sales.service.SalesOrderService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,7 +22,14 @@ public class SalesOrderApiController {
     private final SalesOrderService salesOrderService;
 
     @GetMapping()
-    public List<SalesOrderDto> getSalesOrders(){
+    public List<SalesOrderDto> getSalesOrders() {
         return salesOrderService.getSalesOrders();
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create order", httpMethod = "POST")
+    public ResponseEntity<Void> createProduct(@Valid @RequestBody SalesOrderDto salesOrderDto) {
+        salesOrderService.addSalesOrder(salesOrderDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
