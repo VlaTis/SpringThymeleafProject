230ee9dd3c77b9142e6c3c294ea242682540fe56
Vladas Tiskus <vl.tiskus@gmail.com>
2022-07-23 20:37:20 +0300

    [Security][WiP] Swagger configs and open api created to bypass security

---
+++ vteshop/vteshop-common/src/main/java/eu/codeacademy/vteshop/common/OpenApi.java
@@ -0,0 +1,12 @@
+package eu.codeacademy.vteshop.common;
+
+import java.lang.annotation.*;
+
+/**
+ *
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OpenApi {
+}
